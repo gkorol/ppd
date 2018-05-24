fc572c037115e2044c4b6970c236251f995ecced
ppd59009 <ppd59009@lad.pucrs.br>
2018-05-24 18:58:46 -0300

    vide lista TODO

--- me/mestre_escravo.c
+++ me/mestre_escravo.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "mpi.h"
 
-#define TAM_TRAB 15
+#define TAM_TRAB 10
 
-void ordena(int * vec, int size) {
-  int aux, i, j;
+/*
+*
+*	TODO[OK]: quicksort (sempre com pior caso) do math.h
+*						10K vetores de 100K elementos
+* TODO[OK]: Manter ordem do saco
+* TODO[OK]: Zero copy: mpi_probe
+* TODO: Iniciativa no escravo (após primeiro "round"). Se nao tem trabalho, escravo cai fora; se proc_n-1 pediram trab e nao tinha, mestre cai fora
+* 			Trabalho devolvido pode ser um pedido de novo trabalho...
+* TODO: (relatorio) pode usar ./time e/ou medidas no codigo
+*
+*/
 
-  for(i = 0; i < size; ++i) {
-    for(j = 0; j< size; ++j) {
-      if (vec[i] < vec[j]) {
-	aux = vec[j];
-        vec[j] = vec[i];
-        vec[i] = aux;
-      }
-    }
-  }
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a > *(int*)b );
 }
 
 main(int argc, char** argv)
   {
   int i;
   int j;
-  int my_rank;       // Identificador deste processo
-  int proc_n;        // Numero de processos disparados pelo usuario na linha de comando (np)  
-  int * saco;      // saco de trabalho    
+  int slave;
+  int my_rank;			// Identificador deste processo
+  int proc_n;				// Numero de processos disparados pelo usuario na linha de comando (np)  
+  int * saco;				// saco de trabalho    
   int * message;
   int ret[TAM_TRAB];
   MPI_Status status; // estrutura que guarda o estado de retorno          
@@ -35,75 +37,62 @@ main(int argc, char** argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // pega pega o numero do processo atual (rank)
   MPI_Comm_size(MPI_COMM_WORLD, &proc_n);  // pega informacao do numero de processos (quantidade total)
 
-  srand(time(NULL));
+  if ( my_rank == 0 ) {
 
-  if ( my_rank == 0 ) // qual o meu papel: sou o mestre ou um dos escravos?
-     {
-     // papel do mestre
-     // inicializo o saco de trabalho
-     saco = malloc(proc_n * TAM_TRAB * sizeof(int));
+		saco = malloc( (proc_n-1) * TAM_TRAB * sizeof(int) );
 
-     for( i = 0; i < proc_n; ++i ) {
-       for( j = 0; j < TAM_TRAB; ++j ) {
-	   saco[i*TAM_TRAB+j] = rand()%TAM_TRAB;
-       }
-     }
+		for( i = 0; i < proc_n-1; ++i ) {
+			for( j = 0; j < TAM_TRAB; ++j ) {
+				saco[i*TAM_TRAB+j] = (j-TAM_TRAB)*(-1) + i;
+			}
+		}
 
-     // mostro o saco
-
-     printf("\nMestre[%d]:\n", my_rank);               
-     for( i = 0; i < proc_n; ++i ) {
-       printf("linha %d: ", i);
-       for( j = 0; j < TAM_TRAB; ++j ) {
-	   printf("%3d ",saco[i*TAM_TRAB+j]);
-       }
-       printf("\n");
-     }
+		printf("\nMestre[%d]:\n", my_rank);               
+		for( i = 0; i < proc_n-1; ++i ) {
+			printf("linha %2d: [ ", i);
+			for( j = 0; j < TAM_TRAB; ++j ) {
+				printf("%2d ",saco[i*TAM_TRAB+j]);
+			}
+			printf(" ]\n");
+		}
  
-     // mando o trabalho para os escravos fazerem
+		for ( i=1 ; i < proc_n ; i++) {
+			message = &(saco[(i-1)*TAM_TRAB]);
+			MPI_Send(message, TAM_TRAB, MPI_INT, i, 1, MPI_COMM_WORLD);
+		}
 
-     for ( i=1 ; i < proc_n ; i++)
-         {
-         message = &(saco[(i-1)*TAM_TRAB]);
-         MPI_Send(message, TAM_TRAB, MPI_INT, i, 1, MPI_COMM_WORLD); // envio trabalho saco[i-1] para escravo com id = i
-         }
-     
-     // recebo o resultado
+		printf("\n");
+		for ( i=1 ; i < proc_n ; i++) {			
+			MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			slave = status.MPI_SOURCE;	
 
-// USAR MPI PROBE PARA JA APONTAR O RECV PARA LINHA DO ESCRAVO i 
+			MPI_Recv(&saco[(slave-1)*TAM_TRAB], TAM_TRAB, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		}
+		
+		printf("\n");
 
-     printf("\n");
-     for ( i=1 ; i < proc_n ; i++)
-         {
-         // recebo mensagens de qualquer emissor e com qualquer etiqueta (TAG)
+		// mostro o saco ordenado
+		printf("\nMestre[%d]:\n", my_rank);               
+		for( i = 0; i < proc_n-1; ++i ) {
+			printf("linha %2d: [ ", i);
+			for( j = 0; j < TAM_TRAB; ++j ) {
+				printf("%2d ",saco[i*TAM_TRAB+j]);
+			}
+			printf(" ]\n");
+		}
 
-         MPI_Recv(ret, TAM_TRAB, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+   } else {
+		// papel do escravo
 
-	 printf("Recv de %d: ", status.MPI_SOURCE);
-         for( i = 0; i < TAM_TRAB; ++i ) {
-	   printf("%3d ", ret[i]);
-         }
-         printf("\n");
-
-         }
-     }                 
-  else                 
-     {
-     // papel do escravo
-
-     // inicializo o saco de trabalho
      saco = malloc(TAM_TRAB * sizeof(int));
 
-     // recebo mensagem
-
      MPI_Recv(saco, TAM_TRAB, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-
-     // executo opera��o
-     ordena(saco, TAM_TRAB);
-     // retorno resultado para o mestre
+		 
+		 qsort(saco, TAM_TRAB, sizeof(int), cmpfunc);
 
      MPI_Send(saco, TAM_TRAB, MPI_INT, 0, 1, MPI_COMM_WORLD);
-     }
+
+	}
      
   MPI_Finalize();
 }
