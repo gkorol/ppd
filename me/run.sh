ee29fdb1699de5bf0ef98e2924b0dedc34f8603e
ppd59016 <ppd59016@lad.pucrs.br>
2018-06-05 18:47:57 -0300

    Shell

---
+++ me/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sacos=("100" "500" "1000" "5000" "10000")
+slaves=("2" "4" "8" "16" "32")
+
+for s in ${slaves[@]}
+do
+	for i in ${sacos[@]}
+	do
+		for j in ${sacos[@]}
+		do
+			echo "ladrun -np "$s" mestre_escravo "$i" "$j" > results/output_p"$s"_l"$i"_c"$j".txt"
+		done
+	done
+done
