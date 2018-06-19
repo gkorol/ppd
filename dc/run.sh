ed58bf4967cfbdc29185c025ca9911aaff1cd3c6
11204269 <11204269@facin.portoalegre.pucrsnet.br>
2018-06-19 18:50:11 -0300

    run tudao

--- dc/run.sh
+++ dc/run.sh
@@ -1,26 +1,67 @@
 #!/bin/bash
 
-procs=("63" "127" "255" "511")
-#procs=("16" "32")
-#sacos1=("75000" "90000" "100000")
-#sacos2=("100000" "25000000" "75000000" "100000000")
-sacos1=("100000000")
-deltas=("7500000" "25000000" "20000000" "15000000")
+procs_c=("63" "127" "255" "511")
+procs_a=("15" "31")
+deltas_qs=("7500000" "25000000" "20000000" "15000000")
+deltas_bs=("7500" "25000" "20000" "15000")
 
-for s in ${procs[@]}
+# roda versao A para quick...
+for s in ${procs_a[@]}
 do
-	for i in ${deltas[@]}
+	for i in ${deltas_qs[@]}
 	do
 		echo "Executando p = "$s" com tamanho 1000000000 e delta "$i
-		{ time ladrun -np $s dc_a 1000000000 $i ; } 2> "results/output_p"$s"_s1000000000_d"$i"_bubble.txt"
+		(time ladrun -np $s dc_a_qs 1000000000 $i) > "results/output_a_p"$s"_s1000000000_d"$i"_quick.txt" 2>&1
 	done
 done
 
-#for s in ${procs[@]}
-#do
-#        for i in ${sacos2[@]}
-#        do
-#                echo "Executando p = "$s" com tamanho "$i" e delta 7500000"
-#                ladrun -np $s dc_a $i 7500000 > "results/output_p"$s"_s"$i"_d7500000_bubble.txt"
-#        done
-#done
+# roda versao A para bubble...
+for s in ${procs_a[@]}
+do
+	for i in ${deltas_bs[@]}
+	do
+		echo "Executando p = "$s" com tamanho 1000000 e delta "$i
+		(time ladrun -np $s dc_a_bs 1000000 $i) > "results/output_a_p"$s"_s1000000_d"$i"_bubble.txt" 2>&1
+	done
+done
+
+# roda versao B para quick...
+for s in ${procs_a[@]}
+do
+	for i in ${deltas_qs[@]}
+	do
+		echo "Executando p = "$s" com tamanho 1000000000 e delta "$i
+		(time ladrun -np $s dc_b_qs 1000000000 $i) > "results/output_b_p"$s"_s1000000000_d"$i"_quick.txt" 2>&1
+	done
+done
+
+# roda versao B para bubble...
+for s in ${procs_a[@]}
+do
+	for i in ${deltas_bs[@]}
+	do
+		echo "Executando p = "$s" com tamanho 1000000 e delta "$i
+		(time ladrun -np $s dc_b_bs 1000000 $i) > "results/output_b_p"$s"_s1000000_d"$i"_bubble.txt" 2>&1
+	done
+done
+
+# roda versao C para quick...
+for s in ${procs_c[@]}
+do
+	for i in ${deltas_qs[@]}
+	do
+		echo "Executando p = "$s" com tamanho 1000000000 e delta "$i
+		(time ladrun -np $s dc_a_qs 1000000000 $i) > "results/output_c_p"$s"_s1000000000_d"$i"_quick.txt" 2>&1
+	done
+done
+
+# roda versao C para bubble...
+for s in ${procs_c[@]}
+do
+	for i in ${deltas_bs[@]}
+	do
+		echo "Executando p = "$s" com tamanho 1000000 e delta "$i
+		(time ladrun -np $s dc_a_bs 1000000 $i) > "results/output_c_p"$s"_s1000000_d"$i"_bubble.txt" 2>&1
+	done
+done
+
