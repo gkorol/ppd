7ad9491493200bbc920ee6571ed721f9570b822d
ppd59016 <ppd59016@grad01.lad.pucrs.br>
2018-06-19 18:19:09 -0300

    run.sh atualizado

--- dc/run.sh
+++ dc/run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#procs=("1" "3" "7" "15" "31")
+procs=("63" "127" "255" "511")
 #procs=("16" "32")
-procs=("18" "20" "22" "24" "26" "28" "30" "34" "36" "38" "40")
-sacos1=("75000" "90000" "100000")
-sacos2=("100000" "25000000" "75000000" "100000000")
-# deltas=("1000" "5000")
+#sacos1=("75000" "90000" "100000")
+#sacos2=("100000" "25000000" "75000000" "100000000")
+sacos1=("100000000")
+deltas=("7500000" "25000000" "20000000" "15000000")
 
 for s in ${procs[@]}
 do
-	for i in ${sacos1[@]}
+	for i in ${deltas[@]}
 	do
-		echo "Executando p = "$s" com tamanho "$i" e delta 7500"
-		ladrun -np $s dc_a $i 7500 > "results/output_p"$s"_s"$i"_d7500_bubble.txt"
+		echo "Executando p = "$s" com tamanho 1000000000 e delta "$i
+		{ time ladrun -np $s dc_a 1000000000 $i ; } 2> "results/output_p"$s"_s1000000000_d"$i"_bubble.txt"
 	done
 done
 
